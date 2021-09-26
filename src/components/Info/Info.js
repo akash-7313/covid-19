25723c1166894efb681cc10e54e572e314710d6c
akash-7313 <akashsau79@gmail.com>
2021-09-26 20:14:54 +0600

    final

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -4,7 +4,7 @@ import './Info.css';
 const Info = (props) => {
     // console.log(props.info);
     const { info } = props;
-    // console.log(info);
+    // console.log(info.length);
 
     let hours = 0;
     for (const time of info) {
@@ -17,13 +17,17 @@ const Info = (props) => {
     return (
         <div className="cart">
             <h2><small>Working Hour Info</small></h2>
-            <div className="info">
+            <div>
                 <div className="time">
                     <h2>Total Hours:</h2>
                     <h2><i className="far fa-clock"></i> {hours}</h2>
                 </div>
-                <div>
+                
+                <div className="doctor-list">
                     <h3 className="doctor-name"><small>Doctor's Name : </small></h3>
+                    <h2><i className="fas fa-user-md"></i>{info.length}</h2>
+                </div>
+                <div>
                     <ul><small>{info.map(doctor => <li key={doctor.id}> {doctor.name}</li>)}</small></ul>
                 </div>
             </div>
